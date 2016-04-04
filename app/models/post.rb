e3675ccd4a46e727e4ea82ead63e39a34c883c4b
Kevin McCarthy <kjmagic13@gmail.com>
2016-04-04 13:03:54 -0400

    add attachments to default scope of posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ActiveRecord::Base
 	has_many :attachments, as: :parent, dependent: :destroy
 	accepts_nested_attributes_for :attachments, allow_destroy: true, reject_if: proc { |attributes| attributes[:attachment].nil? }
 
+	default_scope { includes(:attachments) }
 
 end
