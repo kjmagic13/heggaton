ce7816ef5e1bd7b629ae339e2553083801e3ca5f
Kevin McCarthy <kjmagic13@gmail.com>
2016-04-04 17:08:53 -0400

    mock post views

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,4 +3,8 @@ class Attachment < ActiveRecord::Base
 	mount_uploader :attachment, S3Uploader
 	belongs_to :parent, polymorphic: true
 
+	def file(size=nil)
+		self.attachment.url(size)
+	end
+
 end
